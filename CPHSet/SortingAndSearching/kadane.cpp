7e6c1adf038a430243c0f6e927298627c5773665
Vishal Raman <vraman@berkeley.edu>
2020-08-05 23:16:06 -0400

    concertTickets

---
+++ CPHSet/SortingAndSearching/kadane.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef vector<int> vi;
+
+#define FOR(i, a, b) for (ll i = a; i < b; i++)
+#define F0R(i, a) for(ll i = 0; i < a; i++)
+
+const int MOD = 1e9+7; // 998244353;
+const int MX = 2e5+5; //
+const ll INF = 1e18; //
+
+int main() {
+  ios:: sync_with_stdio(0);
+  cin.tie(0);
+  ll curr = -1 * 1e9;
+  ll total = curr;
+  int n;
+   cin >> n;
+   int x [n];
+   F0R(i, n) cin >> x;
+   F0R(i, n){
+     curr = max(curr, 0) + 
+   }
+
+
+  return 0;
+}
---
+++ CPHSet/SortingAndSearching/twoSum.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll,ll> pi;
+typedef vector<int> vi;
+
+#define FOR(i, a, b) for (ll i = a; i < b; i++)
+#define F0R(i, a) for(ll i = 0; i < a; i++)
+
+const int MOD = 1e9+7; // 998244353;
+const int MX = 2e5+5; //
+const ll INF = 1e18; //
+
+bool compare(const pair<int, int>&i, const pair<int, int>&j)
+{
+    return i.first < j.first;
+}
+
+int main() {
+  ios:: sync_with_stdio(0);
+  cin.tie(0);
+  ll n, x;
+  cin >> n >> x;
+  pi a [n];
+  F0R(i, n) {
+    cin >> a[i].first;
+    a[i].second = i+1;
+  }
+  sort(a, a+n, compare);
+
+  int start = 0;
+  int end = n-1;
+  while(start < end){
+    ll val = a[start].first + a[end].first;
+    if (val == x) {
+      cout << a[start].second  << " " << a[end].second << "\n";
+      return 0;
+    } else if (val < x) start++;
+    else end--;
+  }
+  cout << "IMPOSSIBLE" << "\n";
+
+  return 0;
+}
