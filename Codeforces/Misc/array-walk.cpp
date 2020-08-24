722db90d0a1280b98a67a128e25d3349f1461a3d
Vishal Raman <vraman@berkeley.edu>
2020-08-24 00:06:35 -0400

    problems

--- Codeforces/Misc/array-walk.cpp
+++ Codeforces/Misc/array-walk.cpp
@@ -20,15 +20,27 @@ typedef vector<pi> vpi;
 
 
 const int MOD = 1e9+7; // 998244353;
-const int MX = 2e5+5; //
+const int MX = 1e5; //
 const ll INF = 1e18; //
-int t;
+int t, n, k, z, a[MX];
 
 int main() {
   ios:: sync_with_stdio(0);
   cin.tie(0);
   cin >> t;
   while(t--){
+    cin >> n >> k >> z;
+    F0R(i, n) cin >> a[i];
+    vector<vi> score(k+1, vi(z+1, a[0]));
+    for(int i = 1; i <= k; i++) score[i][0] = a[i] + score[i-1][0];
+    for(int j = 1; j <= z; j++){
+      for(int i = 1; i <= k; i++){
+        score[i][j] = max(score[i][j-1], a[i-1] + score[i-1][j-1]);
+      }
+    }
+    F0R(i, z+1) cout << score[k][i] << " ";
+    cout << "\n";
+    // cout << score[k][z] << "\n";
 
   }
 
--- Codeforces/Misc/good-string.cpp
+++ Codeforces/Misc/good-string.cpp
@@ -23,13 +23,31 @@ const int MOD = 1e9+7; // 998244353;
 const int MX = 2e5+5; //
 const ll INF = 1e18; //
 int t;
+string s;
+
+
+int solve(int x, int y){
+  //xyxyxyxy
+  int out = 0;
+  F0R(i, sz(s)){
+    if(s[i] == x + '0'){
+      out++;
+      swap(x, y);
+    }
+  }
+  if(x != y && out % 2) out--; 
+  return out;
+}
 
 int main() {
   ios:: sync_with_stdio(0);
   cin.tie(0);
   cin >> t;
   while(t--){
-
+    cin >> s;
+    int ans = 0;
+    F0R(i, 10) F0R(j, 10) ans = max(ans, solve(i, j));
+    cout << sz(s) - ans << "\n";
   }
 
 
